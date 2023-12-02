56735ffb0e10ed8b53a4805d2e39c3a9d836b84d
Olli Myyryläinen <98151462+ollimyy@users.noreply.github.com>
2023-12-02 14:51:31 +0200

    Day02: CubeConundrum, Part Two.

--- src/day02/CubeConundrum.java
+++ src/day02/CubeConundrum.java
@@ -9,6 +9,12 @@ import java.util.Map;
 public class CubeConundrum {
     public static void main(String[] args) {
         List<String> input = InputUtil.readAllLinesFromFile("src/day02/input.txt");
+
+        System.out.println("Part One: " + partOne(input));
+        System.out.println("Part Two: " + partTwo(input));
+    }
+
+    private static int partOne(List<String> input) {
         int sum = 0;
 
         Map<String, Integer> cubes = new HashMap<>();
@@ -49,6 +55,50 @@ public class CubeConundrum {
             }
         }
 
-        System.out.println(sum);
+        return sum;
+    }
+
+    private static int partTwo(List<String> input) {
+        int sum = 0;
+
+        for (String line : input) {
+            Map<String, Integer> cubes = new HashMap<>();
+            cubes.put("red", 0);
+            cubes.put("green", 0);
+            cubes.put("blue", 0);
+
+            // divide into id and sets
+            String[] gameParts = line.split(":");
+
+            // split the sets
+            String[] sets = (gameParts[1].trim().split(";"));
+
+            // divide the set into "{count}, {color}..."
+            for (String set : sets) {
+                String[] setParts = (set.trim().split(" "));
+
+                // determine the largest amount of each color (minimum set of cubes)
+                for (int i = 0; i < setParts.length; i = i + 2) {
+                    String color = setParts[i + 1].replaceAll("[^a-z]", "");
+                    int cubesOfColor = cubes.get(color);
+                    int count = Integer.parseInt(setParts[i]);
+
+                    if (count > cubesOfColor) {
+                        cubes.put(color, count);
+                    }
+                }
+            }
+
+            // calculate the power of the minimum set
+            int power = 1;
+
+            for (int value : cubes.values()) {
+                power *= value;
+            }
+
+            sum += power;
+        }
+
+        return sum;
     }
 }
